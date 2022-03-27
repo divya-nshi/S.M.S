bb6e0c84a1fe042aa58a44368be47e3a5a63a388
KULSUM <lit2021021@iiitl.ac.in>
2022-03-27 23:01:38 +0530

    added comments

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var express = require("express")//include
-var bodyParser = require("body-parser")
-var mongoose = require("mongoose")
+var express = require("express")//imports express
+var bodyParser = require("body-parser")//imports body-parser
+var mongoose = require("mongoose")//imports mongoose
 
 const app = express()
 
@@ -10,37 +10,37 @@ app.use(bodyParser.urlencoded({
     extended:true
 }))
 
-mongoose.connect('mongodb+srv://kulsum:idklol@cluster0.x8slt.mongodb.net/webd?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://contactus:wNT!m$S7G$.9L.M@cluster0.4qsd4.mongodb.net/web_d?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useUnifiedTopology: true
-});
+});//for establishing the connection to mongodb atlas
 
-var db = mongoose.connection;
+var db = mongoose.connection;//storing connection in a variable db
 
-db.on('error',()=>console.log("Error in Connecting to Database"));
-db.once('open',()=>console.log("Connected to Database"))
+db.on('error',()=>console.log("Error in Connecting to Database"));//if connection not successful
+db.once('open',()=>console.log("Connected to Database"))//if connection succesful
 
 app.post("/sign_up",(req,res)=>{
-    var name = req.body.name;//id same in html file
+    var name = req.body.name;//id same as in html file
     var email = req.body.email;
     var phno = req.body.phno;
     var password = req.body.password;
 
     var data = { //object creation
-        "name": name, 
-        "email" : email,
-        "phone_no": phno,
-        "message" : password
+        "Name": name, 
+        "Email" : email,
+        "Phone_No": phno,
+        "Message" : password
     }
 
-    db.collection('details').insertOne(data,(err,collection)=>{
+    db.collection('details').insertOne(data,(err,collection)=>{//our data will be stored under details
         if(err){
             throw err;
         }
         console.log("Record Inserted Successfully");
     });
 
-    return res.redirect('home_page.html')
+    return res.redirect('home_page.html')//after contact us redirect to next page
 
 })
 
